c284b914c984b54f816abc6efe861c6a1f4f22c0
tanveer-kader <tanveerkaderedu@gmail.com>
2025-10-28 19:32:29 +0800

    build(models): init imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 """Models package for wire detection."""
 
-from .pointnet import PointNet2Detector, PointNet2Backbone
+from .pointnet2 import PointNet2Detector, PointNet2Backbone
 
 __all__ = ['PointNet2Detector', 'PointNet2Backbone']
